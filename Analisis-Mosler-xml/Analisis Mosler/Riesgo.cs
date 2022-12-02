e4fae666bf33828507d5be1b44d40eeba20ef6ee
BryanDariel <107230127+BryanDariel@users.noreply.github.com>
2022-12-02 17:17:30 -0400

    Add files via upload

---
+++ Analisis-Mosler-xml/Analisis Mosler/Riesgo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analisis_Mosler
+{
+    internal class Riesgos
+    {
+        public int ID { get; set; }
+        public string Nombre{ get; set; }
+        public string Descripcion { get; set; }
+        //Espacio :P
+        public string Funcion { get; set; }
+        public string Sustitucion { get; set; }
+        public string Profundidad { get; set; }
+        public string Extension { get; set; }
+        public string Agresion { get; set; }
+        public string Vulnerabilidad { get; set; }
+        //Espacio :P
+        public string Mitigacion { get; set; }
+        public string Nivel { get; set; }
+        public string Total { get; set; }
+
+    }
+}
